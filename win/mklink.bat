51599fddf801f4d1eadbd148af33e14d6da5f783
seino tetsuya <seino.tetsuya@hq.sakicorp.com>
2018-01-15 13:40:07 +0900

    Fixed to Colorscheme

--- win/mklink.bat
+++ win/mklink.bat
@@ -1,3 +1,3 @@
 mklink %userprofile%\_vimrc %userprofile%\.dotfiles\win\_vimrc
 mklink %userprofile%\_gvimrc %userprofile%\.dotfiles\win\_gvimrc
-mklink %userprofile%\dein.toml %userprofile%\.dotfiles\dein.toml
+mklink %userprofile%\dein.toml %userprofile%\.dotfiles\win\dein.toml
