252c520ab4c9f0673bf13c238aa9e48e132e11a2
seino tetsuya <seino.tetsuya@hq.sakicorp.com>
2018-01-17 18:02:18 +0900

    fixed somethings

--- win/mklink.bat
+++ win/mklink.bat
@@ -6,3 +6,5 @@ del /q %userprofile%\dein.toml
 mklink %userprofile%\_vimrc %userprofile%\.dotfiles\win\_vimrc
 mklink %userprofile%\_gvimrc %userprofile%\.dotfiles\win\_gvimrc
 mklink %userprofile%\dein.toml %userprofile%\.dotfiles\win\dein.toml
+
+pause
