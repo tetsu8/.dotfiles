11f45d54944e1f33a71568c28dce9af8d13aae8a
seino tetsuya <seino.tetsuya@hq.sakicorp.com>
2018-01-31 15:44:08 +0900

    add comment and add plugins

--- win/mklink.bat
+++ win/mklink.bat
@@ -1,10 +1,12 @@
 del /q %userprofile%\_vimrc
 del /q %userprofile%\_gvimrc
-del /q %userprofile%\dein.toml
+del /q %userprofile%\.toml
+del /q %userprofile%\.vsvimrc
 
 
 mklink %userprofile%\_vimrc %userprofile%\.dotfiles\win\_vimrc
 mklink %userprofile%\_gvimrc %userprofile%\.dotfiles\win\_gvimrc
-mklink %userprofile%\dein.toml %userprofile%\.dotfiles\win\dein.toml
+mklink %userprofile%\.toml %userprofile%\.dotfiles\win\.toml
+mklink %userprofile%\.vsvimrc %userprofile%\.dotfiles\win\.vsvimrc
 
 pause
